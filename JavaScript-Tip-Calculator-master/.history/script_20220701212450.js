cf7f9af3c5d324205c8ff5edcbdbc3a2a0914fca
HueVu99 <huevu.211001@gmail.com>
2022-07-01 21:25:42 +0700

    uploading website

---
+++ JavaScript-Tip-Calculator-master/.history/script_20220701212450.js
@@ -0,0 +1,42 @@
+
+function calculateTip() {
+
+var billAmount =document.getElementById("billAmount").value;
+var serviceQuality = document.getElementById("serviceQuality").value;
+var numPeople = document.getElementById("totalPeople").value;
+
+if(billAmount==="" || serviceQuality == 0){
+    window.alert("Please enter some values to get this baby up and running!");
+    return; // this will prevent the function from continuing
+}
+
+if(numPeople ===""|| numPeople <=1) {
+    numPeople =1;
+
+    document.getElementById("each").style.display = "none";
+
+} else{
+
+     document.getElementById("each").style.display = "block";
+}
+
+
+var total = (billAmount*serviceQuality)/ numPeople;
+total = Math.round(total*100)/100;
+total= total.toFixed(2);
+
+//Display the tip!
+document.getElementById("totalTip").style.display = "block";
+document.getElementById("tip").innerHTML = total;
+
+}
+
+
+
+//Hide the tip amount on load
+document.getElementById("totalTip"). style.display ="none";
+document.getElementById("each"). style.display = "none";
+
+//Clicking the button calls our custom function
+document.getElementById("calculate").onclick= function(){calculateTip();};
+
